ccdb76f4d2c249236e3f1e73815f8ea83c466abc
LAPTOP-I3K4SCO8\Raul <raulgc1997@gmail.com>
2019-03-21 14:20:07 -0600

    commit ultimas pruebas iteracion 1

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -43,7 +43,7 @@ class ProjectsController < ApplicationController
       respond_to do |format|
         if @project.save
           format.html { redirect_to @project, notice: 'Project was successfully created.' }
-          format.json { render :new, status: :created, location: @project }
+          format.json { render :newproject, status: :created, location: @project }
         else
           format.html { render :new }
           format.json { render json: @project.errors, status: :unprocessable_entity }
