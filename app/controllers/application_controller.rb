178ec3964afa95ab1e0537ad1f265caf8fbb4c69
LAPTOP-I3K4SCO8\Raul <raulgc1997@gmail.com>
2019-03-21 13:28:00 -0600

    commit ultimas pruebas iteracion 1

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  skip_before_filter :verify_authenticity_token
+  
 
   def parser(palabra)
 	  dict_1= {":"=>"D", "/"=>"S","."=>"P","_"=>"R","-"=>"G",","=>"C","&"=>"A","%"=>"V","="=>"E","?"=>"I","@"=>"K",
