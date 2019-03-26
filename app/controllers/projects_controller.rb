00da02d985e7106b94d0710e7d45f58f476aabe3
LAPTOP-I3K4SCO8\Raul <raulgc1997@gmail.com>
2019-03-26 14:23:20 -0600

    commit muestreos

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,7 +1,6 @@
 class ProjectsController < ApplicationController
   before_action :set_project, only: [:show, :edit, :update, :destroy]
   skip_before_action :verify_authenticity_token
-  before_action :set_default_response_format
 
 
   # GET /projects
