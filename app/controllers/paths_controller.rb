c12cc787f90427766390fced91ff35e1c72c9c00
LAPTOP-I3K4SCO8\Raul <raulgc1997@gmail.com>
2019-05-14 01:52:40 -0600

    fechas

--- app/controllers/paths_controller.rb
+++ app/controllers/paths_controller.rb
@@ -19,7 +19,7 @@ class PathsController < ApplicationController
   end
 
   def dates
-    @pathsDates = Path.select(:id, :fecha).where("sampling_id = ?", params[:sampling_id])
+    @pathsDates = Path.all
   end
 
   def datepaths
