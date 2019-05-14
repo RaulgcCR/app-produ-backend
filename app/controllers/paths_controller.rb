4862dc920b655d7a746a6a7dc5736754ca5b5dd1
LAPTOP-I3K4SCO8\Raul <raulgc1997@gmail.com>
2019-05-13 22:30:47 -0600

    fechas unicas

--- app/controllers/paths_controller.rb
+++ app/controllers/paths_controller.rb
@@ -19,7 +19,7 @@ class PathsController < ApplicationController
   end
 
   def dates
-    @pathsDates = Path.select(:id, :fecha).where("sampling_id = ?", params[:sampling_id]).group(:fecha)
+    @pathsDates = Path.select(:id, :fecha).where("sampling_id = ?", params[:sampling_id])
   end
 
   def datepaths
